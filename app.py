16cfe0d838792c10be37742506d4b0f8a4774099
farizziraf <farizziraf3105@gmail.com>
2024-05-07 15:34:22 +0700

    feat: add main app.py

---
+++ app.py
@@ -0,0 +1,44 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+
+# Load the data
+data = pd.read_csv("dataset/tips.csv")
+st.write("Data Visualization using tips.csv Dataset")
+
+# Scatter Plot with Matplotlib
+st.write("Scatter Plot")
+fig, ax = plt.subplots()
+scatter_plot = ax.plot(data['tip'])
+st.pyplot(fig)
+
+# Bar Chart with Matplotlib
+st.write("Bar Chart")
+fig, ax = plt.subplots()
+bar_chart = ax.bar(data['day'], data['tip'])
+st.pyplot(fig)
+
+# Histogram with Matplotlib
+st.write("Histogram")
+fig, ax = plt.subplots()
+histogram = ax.hist(data['total_bill'])
+st.pyplot(fig)
+
+# Line Plot with Seaborn
+st.write("Line Plot")
+fig, ax = plt.subplots(figsize=(10, 6))
+line_plot = sns.lineplot(data=data.drop(['total_bill'], axis=1), ax=ax)
+st.pyplot(fig)
+
+# Bar Plot with Seaborn
+st.write("Bar Plot")
+fig, ax = plt.subplots(figsize=(10, 6))
+bar_plot = sns.barplot(x='day', y='tip', data=data, hue='sex', ax=ax)
+st.pyplot(fig)
+
+# Visualization with Interaction with Plotly
+st.write("Line Chart with Interaction")
+fig = px.line(data, y='tip')
+st.plotly_chart(fig)
