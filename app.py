9b6b016be26dbe22e46e87f1536ec5656bad643a
farizziraf <farizziraf3105@gmail.com>
2024-05-14 14:32:47 +0700

    feat: add text to speech feature

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 import plotly.express as px
+import streamlit as st
+import translators as ts
+import pyttsx3
+import time
 import os
 
 # Load the data
@@ -59,3 +63,42 @@ conn = st.connection("mydb", type="sql", autocommit=True)
 df = conn.query('SELECT EnglishPromotionName, StartDate, EndDate, MaxQty from dimpromotion limit 10;', ttl=600)
 
 st.table(df)
+
+# Initialize the pyttsx3 engine
+engine = pyttsx3.init()
+
+# Function to perform text-to-speech and translation
+def perform_tts_and_translation(text, target_language):
+    # Text-to-speech
+    engine.say(text)
+    engine.runAndWait()
+    time.sleep(2)
+    
+    # Translation
+    st.write("Terjemahan dalam bahasa target:")
+    hasil = ts.translate(text, to_language=target_language, translator='google')
+    st.write(hasil)
+    
+    # Text-to-speech for translated text
+    engine.say(hasil)
+    engine.runAndWait()
+
+# Main Streamlit app
+def main():
+    st.title("Text-to-Speech and Translation App")
+    
+    # Input text
+    text = st.text_area("Masukkan teks yang ingin diucapkan dan diterjemahkan", 
+                        "This company was founded in 2010 by the infamous movie star, Graeme Alexander. Currently, the company worths USD 1 billion according to Forbes report in 2023. What an achievement in just 13 years.")
+    
+    # Target language selection
+    target_language = st.selectbox("Pilih bahasa target untuk menerjemahkan", 
+                                   ["id", "fr", "es", "zh-CN"])
+    
+    # Button to perform TTS and translation
+    if st.button("Proses"):
+        perform_tts_and_translation(text, target_language)
+
+# Run the app
+if __name__ == "__main__":
+    main()
